e4191684c55f389c769e7c3c61cf17cb877dc71c
João Sanson <joaosanson01@gmail.com>
2022-07-25 22:53:24 -0300

    few changes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
+MOVIE_DB_API_KEY = GET YOUR API KEY FROM themoviedb.org
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
-MOVIE_DB_API_KEY = "3c4147639deae1e95a57b163a2d5c50a"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
